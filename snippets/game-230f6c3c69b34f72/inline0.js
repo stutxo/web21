b09c2035ebdabecce1bc3276d5c1383984878041
stum0 <70952638+stum0@users.noreply.github.com>
2023-03-10 01:33:07 +0000

    temp

---
+++ snippets/game-230f6c3c69b34f72/inline0.js
@@ -0,0 +1,7 @@
+
+        export function show_loader() {
+            document.querySelector('.loader').hidden = false;
+        }
+        export function hide_loader() {
+            document.querySelector('.loader').hidden = true;
+        }
